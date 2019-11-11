4c0dee0fa62315ad8ec37615430d0466559ef780
Chris Paul <40892949+MrSun-supercode@users.noreply.github.com>
2019-11-11 10:51:50 +0800

    Add files via upload

---
+++ 1144 The Missing Number (20 分).cpp
@@ -0,0 +1,29 @@
+///*
+//使用map存储，方便查找
+//*/
+//#include<iostream>
+//#include<map>
+//using namespace std;
+//map<int, int>P;
+//const int maxsize = 10000000;//maxsize需要开的大一些
+//int main(void)
+//{
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//	{
+//		int temp;
+//		cin >> temp;
+//		P[temp] = 1;
+//	}
+//	for (int i = 1; i < maxsize; i++)
+//	{
+//		if (!P[i])
+//		{
+//			cout << i<<endl;
+//			break;
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
