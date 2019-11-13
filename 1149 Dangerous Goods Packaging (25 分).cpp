3363f1180fb271151e790155bd5e99e6d41529bd
Chris Paul <40892949+MrSun-supercode@users.noreply.github.com>
2019-11-13 21:19:34 +0800

    Add files via upload

--- 1149 Dangerous Goods Packaging (25 分).cpp
+++ 1149 Dangerous Goods Packaging (25 分).cpp
@@ -1,14 +1,73 @@
+/////*
+////题意：先给出元素之间不能相互存在的对，然后给你m列准备打包的物品判断这些物品是否可以一起携带。
+////一开始想使用map，结果发现同一种物品可能有不止一个的互斥物品，所以改用vector来动态保存其互斥物品
+////注意准备打包的物品要使用set而不能使用vector，因为如果使用vector再进行查找时要调用algorithm中的find函数，时间复杂度为O(n)
+////而set中的find函数时间复杂度为O(logn)，因为其是使用红黑树的结构。
+////*/
+////#include<iostream>
+////#include<set>
+////#include<vector>
+////using namespace std;
+////vector<int>v[99999];
+////int main(void)
+////{
+////	int n, m;
+////	cin >> n >> m;
+////	for (int i = 0; i < n; i++)
+////	{
+////		int str1, str2;
+////		cin >> str1 >> str2;
+////		v[str1].push_back(str2);//在str1中保存str2
+////		v[str2].push_back(str1);//在str2中保存str1
+////	}
+////	for (int i = 0; i < m; i++)
+////	{
+////		int temp;
+////		cin >> temp;
+////		int item;
+////		set<int>s;
+////		bool flag = false;
+////		for (int j = 0; j < temp; j++)
+////		{
+////			cin >> item;
+////			s.insert(item);
+////		}
+////		for (auto it = s.begin(); it != s.end(); it++)
+////		{
+////			if (v[*it].size()!=0)//如果当前s中的元素有互斥物品
+////			{
+////				for (int i = 0; i < v[*it].size(); i++)//遍历其互斥物品，看s中是否有其互斥物品
+////				{
+////					if (s.find(v[*it][i]) != s.end())//若有，则输出No,并直接跳出循环
+////					{
+////						cout << "No" << endl;
+////						flag = true;
+////						break;
+////					}
+////				}	
+////				if (flag)
+////				{
+////					break;
+////				}
+////			}
+////		}
+////		if (!flag)
+////		{
+////			cout << "Yes" << endl;
+////		}
+////	}
+////	system("pause");
+////	return 0;
+////}
 ///*
-//题意：先给出元素之间不能相互存在的对，然后给你m列准备打包的物品判断这些物品是否可以一起携带。
-//一开始想使用map，结果发现同一种物品可能有不止一个的互斥物品，所以改用vector来动态保存其互斥物品
-//注意准备打包的物品要使用set而不能使用vector，因为如果使用vector再进行查找时要调用algorithm中的find函数，时间复杂度为O(n)
-//而set中的find函数时间复杂度为O(logn)，因为其是使用红黑树的结构。
+//柳婼大佬：使用map数据结构结合vector保存，比我的方法好，我的方法要开v【99999】的vector数组
 //*/
 //#include<iostream>
 //#include<set>
+//#include<map>
 //#include<vector>
 //using namespace std;
-//vector<int>v[99999];
+//map<int, vector<int>>P;
 //int main(void)
 //{
 //	int n, m;
@@ -17,8 +76,8 @@
 //	{
 //		int str1, str2;
 //		cin >> str1 >> str2;
-//		v[str1].push_back(str2);//在str1中保存str2
-//		v[str2].push_back(str1);//在str2中保存str1
+//		P[str1].push_back(str2);//在str1中保存str2
+//		P[str2].push_back(str1);//在str2中保存str1
 //	}
 //	for (int i = 0; i < m; i++)
 //	{
@@ -34,17 +93,17 @@
 //		}
 //		for (auto it = s.begin(); it != s.end(); it++)
 //		{
-//			if (v[*it].size()!=0)//如果当前s中的元素有互斥物品
+//			if (P[*it].size() != 0)//如果当前s中的元素有互斥物品
 //			{
-//				for (int i = 0; i < v[*it].size(); i++)//遍历其互斥物品，看s中是否有其互斥物品
+//				for (int i = 0; i < P[*it].size(); i++)//遍历其互斥物品，看s中是否有其互斥物品
 //				{
-//					if (s.find(v[*it][i]) != s.end())//若有，则输出No,并直接跳出循环
+//					if (s.find(P[*it][i]) != s.end())//若有，则输出No,并直接跳出循环
 //					{
 //						cout << "No" << endl;
 //						flag = true;
 //						break;
 //					}
-//				}	
+//				}
 //				if (flag)
 //				{
 //					break;
